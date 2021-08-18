0ea173b59ec64279a8681735d0851951256bda94
sphills <sage242@hotmail.com>
2021-08-18 09:55:09 -0600

    Added validation
    Added the checkValid() method to return a boolean for the validity of the input number. If checkValid() returns false, then  spotlightSum() doesn't calculate the spotlightSumValue and outputs "That number was out of bounds." to the console

--- src/edabitChallenges/SpotlightSumAttempt_01.java
+++ src/edabitChallenges/SpotlightSumAttempt_01.java
@@ -9,8 +9,8 @@ public class SpotlightSumAttempt_01 {
 		int targetValueElement = 0;
 		int spotlightSumValue = 0;
 		
-		for (int i = 0; i < (numberGrid.length - 1); i++) {
-			for (int j = 0; j < (numberGrid[i].length - 1); j++) {
+		for (int i = 0; i < (numberGrid.length); i++) {
+			for (int j = 0; j < (numberGrid[i].length); j++) {
 				if (numberGrid[i][j] == spotlightNumberCenter) {
 				 	targetValueRow = i;
 				 	targetValueElement = j;
@@ -18,17 +18,22 @@ public class SpotlightSumAttempt_01 {
 			}
 		}
 		
-		System.out.println("Target row is: " + targetValueRow + ", and target element is: " + targetValueElement);
-
-		for (int i = -1; i < 2; i++) {
-			for (int j = -1; j < 2; j++){
-				spotlightSumValue += numberGrid[targetValueRow + i][targetValueElement + j];
+		if (checkValid(targetValueRow, targetValueElement)) {
+			System.out.println("Target row is: " + targetValueRow + ", and target element is: " + targetValueElement);
+	
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++){
+					spotlightSumValue += numberGrid[targetValueRow + i][targetValueElement + j];
+				}
 			}
+		} else {
+			System.out.println("That number was out of bounds.");
 		}
 		
 		return spotlightSumValue;
 	}
 
+	// This constructor fills the numberGrid with 1 - 100 so that spotlightSum() can run
 	public SpotlightSumAttempt_01() {
 		
 		for (int i = 0; i < numberGrid.length; i++) {
@@ -36,5 +41,24 @@ public class SpotlightSumAttempt_01 {
 				numberGrid[i][j] = ((j) + ((i * 10) + 1));
 			}
 		}
+	}
+	
+	/* This method validates the given inputs and returns a boolean that spotlightSum() uses to determine
+	 * whether or not to use the targetRowValue or targetRowElement values.
+	 */
+	private boolean checkValid(int inputValueRow, int inputValueElement) {
+		boolean isValueInput = true;
+		
+		if (inputValueRow == 0 || inputValueRow == 9) {
+			System.out.println("Sorry, that number doesn't have 8 surrounding numbers.");
+			isValueInput = false;
+		} else {
+			if (inputValueElement == 0 || inputValueElement == 9) {
+				System.out.println("Sorry, that number doesn't have 8 surrounding numbers.");
+				isValueInput = false;
+			}
+		}
+		
+		return isValueInput;
 	}
 }
--- src/edabitChallenges/SpotlightSumTester.java
+++ src/edabitChallenges/SpotlightSumTester.java
@@ -6,5 +6,10 @@ public class SpotlightSumTester {
 		SpotlightSumAttempt_01 testAttempt = new SpotlightSumAttempt_01();
 		
 		System.out.println(testAttempt.spotlightSum(45));
+		System.out.println(testAttempt.spotlightSum(65));
+		System.out.println(testAttempt.spotlightSum(99));
+		System.out.println(testAttempt.spotlightSum(1));
+		System.out.println(testAttempt.spotlightSum(89));
+		System.out.println(testAttempt.spotlightSum(100));
 	}
 }
