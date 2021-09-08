88e4b34a7ce78c37ab32c3a3bb273764f8f1627f
sphills <sage242@hotmail.com>
2021-09-08 11:34:48 -0500

    Manual Array sort()
    I implemented a nested loop array sort in this update, and added print() to each of the methods so the Driver doesn't have to call println() and their return.

--- src/arrayMinMaxDifference/ArrayMinMaxCalculator.java
+++ src/arrayMinMaxDifference/ArrayMinMaxCalculator.java
@@ -5,8 +5,28 @@ import java.util.Arrays;
 public class ArrayMinMaxCalculator {
 
 	public int differenceMaxMin(int[] inputArray) {
-		int[] sortedArray = inputArray; // A local int[] instance because I'm not sure if sorting on the inputArray will work the way I want.
-		Arrays.sort(sortedArray);
-		return (sortedArray[sortedArray.length - 1] - sortedArray[0]);
+		Arrays.sort(inputArray);
+		System.out.print("The difference between the largest and smallest value in this array is: " + (inputArray[inputArray.length - 1] - inputArray[0]) + "\n");
+		return (inputArray[inputArray.length - 1] - inputArray[0]);
+	}
+	
+	public int sortAndDifferenceMaxMin(int[] inputArray) {
+		int sortHolder = 0;
+		
+		for (int i = 0; i < inputArray.length; i++) {
+			for (int j = i+1; j < inputArray.length; j++) {
+				if (inputArray[i] > inputArray[j]) {
+					sortHolder = inputArray[i];
+					inputArray[i] = inputArray[j];
+					inputArray[j] = sortHolder;
+				}
+			}
+			
+			System.out.println(inputArray[i]);
+		}
+		
+		System.out.print("The difference between the largest and smallest value in this array is: " + (inputArray[inputArray.length - 1] - inputArray[0]) + "\n");
+		
+		return (inputArray[inputArray.length - 1] - inputArray[0]);
 	}
 }
--- src/arrayMinMaxDifference/ArrayMinMaxDriver.java
+++ src/arrayMinMaxDifference/ArrayMinMaxDriver.java
@@ -4,8 +4,8 @@ public class ArrayMinMaxDriver {
 
 	public static void main(String[] args) {
 		ArrayMinMaxCalculator testCalculator = new ArrayMinMaxCalculator();
-		System.out.println(testCalculator.differenceMaxMin(new int[] {-1, 0, 1}));
-		System.out.println(testCalculator.differenceMaxMin(new int[] {-10, 50, 137}));
-		System.out.println(testCalculator.differenceMaxMin(new int[] {1, 574, -3858, 59, -5, 4827}));
+		testCalculator.differenceMaxMin(new int[] {-1, 0, 1});
+		testCalculator.differenceMaxMin(new int[] {-10, 50, 137});
+		testCalculator.sortAndDifferenceMaxMin(new int[] {1, 574, -3858, 59, -5, 4827});
 	}
 }
