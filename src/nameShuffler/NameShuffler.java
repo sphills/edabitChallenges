4d2663a235e7d8cdc00ac3feac43c4580fb09d35
sphills <sage242@hotmail.com>
2021-09-08 13:24:18 -0500

    nameShuffle() Refactored again
    I realized that I didn't even need to make String firstName, lastName to return the proper string, so the final nameShuffle() is significantly more brief while still returning correctly

--- src/nameShuffler/NameShuffler.java
+++ src/nameShuffler/NameShuffler.java
@@ -5,9 +5,7 @@ public class NameShuffler {
 	public String nameShuffle(String fullName) {
 		
 		String[] splitFullName = fullName.split(" ");
-		String firstName = splitFullName[0];
-		String lastName = splitFullName[1];
 		
-		return lastName + " " + firstName;
+		return splitFullName[1] + " " + splitFullName[0];
 	}
 }
