7fa41ba4bc6eea86f0462056a76b118465f57e43
sphills <sage242@hotmail.com>
2021-09-08 11:17:36 -0500

    Added Arrays.sort()
    I forgot to add the Arrays.sort() method previously and the returned value was correct by chance. This update I added in the sort() method and now it runs correctly.

--- src/arrayMinMaxDifference/ArrayMinMaxCalculator.java
+++ src/arrayMinMaxDifference/ArrayMinMaxCalculator.java
@@ -1,9 +1,12 @@
 package arrayMinMaxDifference;
 
+import java.util.Arrays;
+
 public class ArrayMinMaxCalculator {
 
 	public int differenceMaxMin(int[] inputArray) {
 		int[] sortedArray = inputArray; // A local int[] instance because I'm not sure if sorting on the inputArray will work the way I want.
-		return (sortedArray[sortedArray.length-1] - sortedArray[0]);
+		Arrays.sort(sortedArray);
+		return (sortedArray[sortedArray.length - 1] - sortedArray[0]);
 	}
 }
--- src/arrayMinMaxDifference/ArrayMinMaxDriver.java
+++ src/arrayMinMaxDifference/ArrayMinMaxDriver.java
@@ -6,5 +6,6 @@ public class ArrayMinMaxDriver {
 		ArrayMinMaxCalculator testCalculator = new ArrayMinMaxCalculator();
 		System.out.println(testCalculator.differenceMaxMin(new int[] {-1, 0, 1}));
 		System.out.println(testCalculator.differenceMaxMin(new int[] {-10, 50, 137}));
+		System.out.println(testCalculator.differenceMaxMin(new int[] {1, 574, -3858, 59, -5, 4827}));
 	}
 }
