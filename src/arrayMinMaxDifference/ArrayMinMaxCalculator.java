bc01e3f598749a780b3ce6a4ac253ceacc8a1f00
sphills <sage242@hotmail.com>
2021-09-08 11:14:30 -0500

    ArrayMinMax first pass
    This is the prototyped code made functional with a driver class. As of now, returns the proper integer that measures the difference between the largest and the smallest values in the array.

--- src/arrayMinMaxDifference/ArrayMinMaxCalculator.java
+++ src/arrayMinMaxDifference/ArrayMinMaxCalculator.java
@@ -2,4 +2,8 @@ package arrayMinMaxDifference;
 
 public class ArrayMinMaxCalculator {
 
+	public int differenceMaxMin(int[] inputArray) {
+		int[] sortedArray = inputArray; // A local int[] instance because I'm not sure if sorting on the inputArray will work the way I want.
+		return (sortedArray[sortedArray.length-1] - sortedArray[0]);
+	}
 }
--- src/arrayMinMaxDifference/ArrayMinMaxDriver.java
+++ src/arrayMinMaxDifference/ArrayMinMaxDriver.java
@@ -2,4 +2,9 @@ package arrayMinMaxDifference;
 
 public class ArrayMinMaxDriver {
 
+	public static void main(String[] args) {
+		ArrayMinMaxCalculator testCalculator = new ArrayMinMaxCalculator();
+		System.out.println(testCalculator.differenceMaxMin(new int[] {-1, 0, 1}));
+		System.out.println(testCalculator.differenceMaxMin(new int[] {-10, 50, 137}));
+	}
 }
