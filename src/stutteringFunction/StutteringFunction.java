7742497f989fc6c8738c0bf60cbb7e44c10d5536
sphills <sage242@hotmail.com>
2021-09-13 15:37:32 -0500

    StutteringFunction initial commit
    This is the inital commit of StutteringFunction(Driver/Notes) with the notes filled in, with prompt, pre-activity thoughts, and an untested prototype.

---
+++ src/stutteringFunction/StutteringFunction.java
@@ -0,0 +1,6 @@
+package stutteringFunction;
+
+public class StutteringFunction {
+
+	
+}
---
+++ src/stutteringFunction/StutteringFunctionDriver.java
@@ -0,0 +1,8 @@
+package stutteringFunction;
+
+public class StutteringFunctionDriver {
+
+	public static void main(String[] args) {
+		StutteringFunction testFunction = new StutteringFunction();
+	}
+}
