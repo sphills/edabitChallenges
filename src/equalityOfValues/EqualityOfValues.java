235b411226f04e821ead1ad00005d5460ed1f918
sphills <sage242@hotmail.com>
2021-09-13 16:48:40 -0500

    EqualityOfValues final commit
    I made equal() as lean as possible: it returns a nested ternary operator value. It's not the easiest thing to read, but by golly is it concise. Funnily, no other responses on the solution page are anywhere near as short, but obviously they're almost all much easier to parse/read at a glance. That was fun!

--- src/equalityOfValues/EqualityOfValues.java
+++ src/equalityOfValues/EqualityOfValues.java
@@ -3,16 +3,6 @@ package equalityOfValues;
 public class EqualityOfValues {
 
 	public int equal(int a, int b, int c) {
-
-		int returnInt = 0;
-
-		if (a == b) {
-			returnInt = (b == c) ? 3: 2;
-			
-		} else {
-			returnInt = (b == c) ? 2: (a == c) ? 2: 0; 
-		}
-		
-		return returnInt;
+		return ((a == b) ? ((b == c) ? 3: 2): ((b == c) ? 2: (a == c) ? 2: 0));
 	}
 }
